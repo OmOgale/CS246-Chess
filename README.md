846d23eb7a6dae5b63a39f3c090049ff9e57cc0e
OmOgale <123871192+OmOgale@users.noreply.github.com>
2024-04-04 00:55:51 -0400

    Adding more desc

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CS246-Chess
+<h1 align="center">Chess</h1>
 <p align="center">
-<img src="/assets/Chess.jpeg" />
+<img src="/assets/Chess.jpeg" height="500px"/>
 </p>
---
+++ main.cc
@@ -0,0 +1,9 @@
+# include <iostream>
+
+int main() {
+    std::cout << "This game was created as a final project for the CS246 Fall 2023 offering at the University of Waterloo." << std::endl;
+    std::cout << "This project was spearheaded by Om Ogale, Aarav Jain, and Nivedita Prabhu." << std::endl;
+    std::cout << "Designed using OOP practices, algorithms such as minimax as well as software design patterns such as MVC." << std::endl;
+    std::cout << "Several documents such as UML Diagrams as well as design files were prepared before undertaking this project." << std::endl;
+    std::cout << "However due to academic policy, the code as well as the documents concerned cannot be made public" << std::endl;
+}
